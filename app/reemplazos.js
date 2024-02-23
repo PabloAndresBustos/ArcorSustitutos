d9509caae4f34dd8490bd4a813c48c51e5158506
PabloAndresBustos <pabloandresbustos@gmail.com>
2024-02-23 13:52:00 -0300

    Agregado de codigos reemplazados

--- app/reemplazos.js
+++ app/reemplazos.js
@@ -36,8 +36,6 @@ async function reemplazo(lista, info) {
                 lista[i] = codigosNuevos[j];
                 info.push("El código " + codigosViejos[j] +
                 " se reemplazó por el: " + codigosNuevos[j]);
-            }else{
-                info.push("Este archivo no tiene codigos a reemplazar");
             }
         }
     }
